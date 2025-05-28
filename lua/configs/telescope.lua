e07a0343312a5d20a58d1205f2e3bd10f4af2138
Skylar Abruzese <skylar@abruzese.com>
2025-05-28 14:54:06 -0400

    added files

---
+++ lua/configs/telescope.lua
@@ -0,0 +1,40 @@
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+local options = {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        ["<esc>"] = actions.close,
+      },
+    },
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+    },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    }
+  }
+}
+
+telescope.setup(options)
+
+-- Load extensions
+telescope.load_extension("fzf")
+telescope.load_extension("neoclip")
+
+return options
